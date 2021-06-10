fe3fbccb1c224d53c4764b251d3d519275af60c0
Olivier Prouvost <olivier.prouvost@opcoach.com>
2021-06-10 14:16:28 +0200

    Fix handler migraiton

--- com.opcoach.training.rental.ui/src/com/opcoach/training/rental/ui/RentalAddon.java
+++ com.opcoach.training.rental.ui/src/com/opcoach/training/rental/ui/RentalAddon.java
@@ -1,8 +1,11 @@
 package com.opcoach.training.rental.ui;
 
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 
 import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.ui.di.UIEventTopic;
 
 import com.opcoach.training.rental.RentalAgency;
 import com.opcoach.training.rental.core.RentalCoreActivator;
@@ -13,5 +16,12 @@ public class RentalAddon {
 	public void initializeRental(IEclipseContext ctx) {
 		
 		   ctx.set(RentalAgency.class, RentalCoreActivator.getAgency());
+	}
+	
+	
+	@Inject @Optional
+	public void receiveevent(@UIEventTopic("eventTest") String z)
+	{
+		System.out.println(" I received this event : " + z);
 	}
 }
--- com.opcoach.training.rental.ui/src/com/opcoach/training/rental/ui/e4/handlers/HelloWorldHandler.java
+++ com.opcoach.training.rental.ui/src/com/opcoach/training/rental/ui/e4/handlers/HelloWorldHandler.java
@@ -4,14 +4,16 @@ package com.opcoach.training.rental.ui.e4.handlers;
 import javax.inject.Named;
 
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
 
 public class HelloWorldHandler {
 	@Execute
-	public void execute(@Named(IServiceConstants.ACTIVE_SHELL) Shell s) {
+	public void execute(@Named(IServiceConstants.ACTIVE_SHELL) Shell s, IEventBroker ieb) {
 		MessageDialog.openInformation(s, "Hello world", " Hello world message");
+		ieb.send("eventTest", "Hello");
 	}
 		
 }
